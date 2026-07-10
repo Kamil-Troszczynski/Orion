009435c8e7e140b39e2f536ee932a55808f12ddd
Kamil-Troszczynski <troszczynskikamil@outlook.com>
2026-07-10 13:48:28 +0200

    quick fix

--- src/pipeline.py
+++ src/pipeline.py
@@ -77,7 +77,6 @@ async def run_bot(transport: BaseTransport, runner_args: RunnerArguments):
             stt,
             user_aggregator,
             llm,
-            
             tts,
             transport.output(),
             assistant_aggregator,
